17e19aeebe70f0eff9012cc3148ec1f52c4618b2
daracoo <dkuzmanoski21@gmail.com>
2024-08-08 15:35:06 +0200

    Added question's logic whether the answer is correct or wrong

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,8 @@
-import { useState, useCallback } from "react"
+import { useState, useCallback, act } from "react"
 
 import QUESTIONS from '../questions.js'
 import quizCompleteImg from '../assets/quiz-complete.png'
-import QuestionTimer from "./QuestionTimer.jsx";
+import Question from "./Question.jsx";
 
 export default function Quiz(){
     const [userAnswers, setUserAnswers] = useState([]);
@@ -25,23 +25,12 @@ export default function Quiz(){
         </div>
     }
 
-    const schuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-    schuffledAnswers.sort(() => Math.random() - 0.5);
 
     return <div id="quiz">
-        <div id="question">
-            <QuestionTimer
-            key={activeQuestionIndex} 
-            timeout={10000} 
-            onTimeout={() => {handleSkipAnswer}} />
-            <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-            <ul id="answers">
-                {schuffledAnswers.map((answer) => (
-                    <li key={answer} className="answer">
-                    <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-                    </li>
-                ))}
-            </ul>
-        </div>
+        <Question
+        key={activeQuestionIndex}
+        index={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer} 
+        onSkipAnswer={handleSkipAnswer}/>
     </div> 
 }
